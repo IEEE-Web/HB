1723139c8e918e4b00595c7749bdbfcf473d7264
Moataz Alaa <mutazalaasalah@gmail.com>
2021-02-16 13:52:47 +0200

    Creaing structure

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import "./header.css"
+export default function Header() {
+    return (
+            <div className="card">
+                <img src="https://raw.githubusercontent.com/DenverCoder1/Responsive-Birthday-Card/main/birthday.svg" alt="birthday" className="birthday"/>
+                <div className="text">
+                    <h1>Eng.Hager <br />Happy Birthday!</h1>
+                    <p>I hope all your birthday wishes and dreams come true with the <br /><span>New Family</span></p>
+                </div>
+                <div className="space"></div>
+                </div>
+ 
+    )
+}
