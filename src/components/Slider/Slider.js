610b105e6a6024041cf5274860ad0c53fc806ec6
Moataz Alaa <mutazalaasalah@gmail.com>
2021-02-16 20:12:25 +0200

    add messages into file

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,48 +2,28 @@ import React from 'react'
 import "./slider.css"
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
-import { Player } from  "video-react";
 import vid from "./assets/vid.mp4"
-
-import img from "./assets/mm.jpg"
+import {messages} from "./messages"
 export default function Slider() {
     const slider = (
         <AwesomeSlider animation="cubeAnimation">
-            <div className="sliderContainer">
-          <div className="allSlider" >
-              <div className="containerr">
-              <div className="container-black" dir="rtl">
-            
-              </div>
-          </div>
+                {
+                    messages.map(message=>{
+                        return(
+                            <div className="allSlider" >
+                                <div className="containerr">
+                                    <div className="container-black" dir="rtl">
+                                        <p>
+                                        {message.message}
+                                        </p>
+                                    </div>
+                                </div>
           
-              <h2>Moataz Alaa</h2>
-          </div>
-          </div>
-          <div className="allSlider">
-              <div className="containerr">
-              <div className="container-black_img">
-                
-              </div>
-          </div>
-              
-          </div>
-          <div className="allSlider">
-              <div className="containerr">
-              <div className="container-black">
-              1
-              </div>
-          </div>
-              <h2>Moataz Alaa</h2>
-          </div>
-          <div className="allSlider">
-              <div className="containerr">
-              <div className="container-black">
-              1
-              </div>
-          </div>
-              <h2>Moataz Alaa</h2>
-          </div>
+                                    <h2>{message.nameSender}</h2>
+                             </div>
+                        )
+                    })
+                }
         </AwesomeSlider>
       );
     return (
