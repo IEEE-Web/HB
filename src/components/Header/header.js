fde34416b328c73a11d30262552593f8f602cbb5
Moataz Alaa <mutazalaasalah@gmail.com>
2021-02-20 16:48:43 +0200

    handle responsive

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -9,11 +9,11 @@ export default function Header() {
    
     return (
             <div className="card">
-                <img src="https://raw.githubusercontent.com/DenverCoder1/Responsive-Birthday-Card/main/birthday.svg" alt="birthday" className="birthday"/>
                 <div className="text">
                     <h1>Eng.Hager <br />Happy Birthday!</h1>
                     <p>I hope all your birthday wishes and dreams come true with the <br /><span>New Family</span></p>
                 </div>
+                <img src="https://raw.githubusercontent.com/DenverCoder1/Responsive-Birthday-Card/main/birthday.svg" alt="birthday" className="birthday"/>
                 <div className="space"></div>
                 
                 </div>
